56cacfae1ebfe5b8b1d5f85a2c86e3a320b6e6f7
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-11-08 20:04:51 +0530

    basic response based on url visited

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require("http")
 
 const server = http.createServer((req, res)=>{
-  console.log("URL:");
-  console.log(req.url);
-  console.log("Method:");
-  console.log(req.method);
-  console.log("Headers:");
-  console.log(req.headers);
+  console.log(req.url, req.method, req.headers);
+  const url = req.url
+  if(url === "/"){
+    res.write("<body><h1>This is response from server</h1></body>")
+    res.write("<body><input action='/message' method='POST' name='msg' type='text'><button type='submit'>submit</button></body>")
+    return res.end()
+  }
   res.setHeader("Content-Type", 'text/html')
   res.write("<body><h1>This is response from server</h1></body>")
   res.end()
