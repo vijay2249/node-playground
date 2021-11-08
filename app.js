da8e928520592e433e8e58bf7dcbdd5a635a8bc1
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-11-08 20:47:35 +0530

    basic route just nodejs

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const http = require("http")
+const fs = require("fs")
 
 const server = http.createServer((req, res)=>{
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
   const url = req.url
+  const method = req.method
   if(url === "/"){
+    console.log("inside the / route");
     res.write("<body><h1>This is response from server</h1></body>")
-    res.write("<body><input action='/message' method='POST' name='msg' type='text'><button type='submit'>submit</button></body>")
+    res.write("<body><form action='/message' method='POST'><input name='msg' type='text'><button type='submit'>submit</button></form></body>")
+    return res.end()
+  }
+  if(url === "/message" && method === "POST"){
+    console.log("Inside the /message route");
+    fs.writeFileSync("msg.txt", "DUMMY text")
+    res.statusCode = 302
+    res.setHeader("Location", "/")
     return res.end()
   }
   res.setHeader("Content-Type", 'text/html')
