c2d35b13f9d3a0aad42e27456d022420753a2423
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-11-08 19:53:37 +0530

    server response html code writing pattern

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const http = require("http")
 
 const server = http.createServer((req, res)=>{
-  console.log(req);
-  process.exit()
+  console.log("URL:");
+  console.log(req.url);
+  console.log("Method:");
+  console.log(req.method);
+  console.log("Headers:");
+  console.log(req.headers);
+  res.setHeader("Content-Type", 'text/html')
+  res.write("<body><h1>This is response from server</h1></body>")
+  res.end()
+  // process.exit()
 })
 
-server.listen(3000)
+server.listen(3000, ()=>{
+  console.log("Server started in port 3000");
+})
