ad8c9bee82cf5f6785fbbb0ab631752bb46627e4
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-11-09 19:28:20 +0530

    module exports method-1

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
 const http = require("http")
-const fs = require("fs")
 
-const server = http.createServer((req, res)=>{
-  // console.log(req.url, req.method, req.headers);
-  const url = req.url
-  const method = req.method
-  if(url === "/"){
-    res.write("<body><h1>This is response from server</h1></body>")
-    res.write("<body><form action='/message' method='POST'><input name='msg' type='text'><button type='submit'>submit</button></form></body>")
-    return res.end()
-  }
-  else if(url === "/message" && method === "POST"){
-    const body_data = []
-    req.on('data', (chunk)=>{
-      console.log(chunk);
-      body_data.push(chunk)
-    })
-    return req.on('end', ()=>{
-      const data = Buffer.concat(body_data).toString()
-      console.log(data);
-      const message = data.split("=")[1]
-      // fs.writeFileSync("msg.txt", message)
-      // res.statusCode = 302
-      // res.setHeader("Location", "/")
-      // return res.end()
-      fs.writeFile("msg.txt", message, err=>{
-        if(!err){
-          res.statusCode = 302
-          res.setHeader("Location", "/")
-          return res.end()
-        }
-      })
-    })
-  }
-  res.setHeader("Content-Type", 'text/html')
-  res.write("<body><h1>This is response from server</h1></body>")
-  res.end()
-  // process.exit()
-})
+const requestHandler = require("./routes")
+
+const server = http.createServer(requestHandler)
 
 server.listen(3000, ()=>{
   console.log("Server started in port 3000");
