b8716a32fa50cbb713513ffe0cb7cddc8c511d4c
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-11-11 19:24:44 +0530

    app.use() use cases

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+// core modules
 const http = require("http")
 
-const requestHandler = require("./routes")
+// third party modules
+const express = require("express")
 
-const server = http.createServer(requestHandler.routesHandler)
+const app = express()
+
+app.use((req, res, next)=>{
+  console.log("This is first app.use function");
+  next(); // used to execute the next app function the runs the logic or routing or anything else
+})
+
+app.use((req, res, next)=>{
+  console.log("This is second app.use function");
+  next();
+})
+
+const server = http.createServer(app)
 
 server.listen(3000, ()=>{
   console.log("Server started in port 3000");
